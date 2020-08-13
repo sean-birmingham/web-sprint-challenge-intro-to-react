90ae38ef2efee3801faa6beb389f895a23bf0dff
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-13 10:22:31 -0400

    fetching data from swapi and setting state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = [];
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then((response) => {
+        setCharacters(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className='App'>
+      <h1 className='Header'>Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
